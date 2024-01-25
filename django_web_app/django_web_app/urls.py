3cefd3052af34bda5a5b92692af8cb5fcb5b0644
IsaacBraamGit <78974377+IsaacBraamGit@users.noreply.github.com>
2024-01-25 15:47:52 +0100

    removed crispyforms, works now

--- django_web_app/django_web_app/urls.py
+++ django_web_app/django_web_app/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
